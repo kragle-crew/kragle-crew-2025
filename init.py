639dcbc45a5dbd7b8303a985b2aa6c4a7924e594
Vesk <avesky@gmail.com>
2025-10-15 20:05:14 -0500

    session work

--- init.py
+++ init.py
@@ -1,10 +1,13 @@
 from pybricks.hubs import PrimeHub
 from pybricks.robotics import DriveBase
-from pybricks.pupdevices import Motor
+from pybricks.pupdevices import Motor, ForceSensor
 from pybricks.parameters import Port, Direction
 
 hub = PrimeHub()
 left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor, 57.5, 100)
+lift = Motor(Port.D)
+pressure = ForceSensor(Port.C)
+
 print("init complete")
--- tasks/taskA.py
+++ tasks/taskA.py
@@ -1,4 +1,4 @@
-from init import robot
+from init import robot,lift,pressure
 
 # async def square():
 #     await robot.straight(400)
@@ -14,12 +14,24 @@ from init import robot
 #     await robot.straight(400)
 #     await robot.turn(-90)
 
-
+async def liftGoDown():
+    await lift.run_target (400,0)
 async def task():
-    await robot.turn(360)
-    robot.settings(straight_speed=500)
+    #await robot.turn(360)
+    robot.settings(straight_speed=200)
+    
+    # await lift.run_angle(360,360)
+
+    await lift.run_target(400,-950,)
+    await robot .straight(60)
+    await robot.turn (30)
+    await robot.turn (-60)
+    await robot.turn (30)
+    await robot.straight (-60)
+    await liftGoDown()
     # await robot.curve(300,-80)
     # await square()
-    # await square()
-    # await square()
 
+    # await square()
+    # await square()
+    
