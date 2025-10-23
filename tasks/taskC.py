687bf6945c7581933ef0a269635ba1562a420ce0
Andreas Christianson <achristianson@pessimistic-it.com>
2025-10-22 20:09:37 -0500

    nandita code

--- tasks/taskC.py
+++ tasks/taskC.py
@@ -1,8 +1,24 @@
 from init import robot
+from utilities.lift import reset_lift, lift_to_height
+from pybricks.tools import multitask
 
 
 async def task():
-  robot.settings(speed=500)
-  for value in range(0,2000):
-    await robot.straight(1000)
+    await multitask(
+      robot.straight(300), 
+      reset_lift()
+      )
+    robot.settings(straight_speed=328)
+    await robot.turn(-45)
+    await multitask(
+      robot.straight(290), 
+      lift_to_height(98)
+      )
+    await robot.turn(-90)
+    await robot.straight(128)
+    await robot.curve(-140, 140)
+    await robot.turn(-106)
+    await robot.straight(58)
     await robot.turn(90)
+    await robot.straight(82)
+    await robot.turn(-2)
