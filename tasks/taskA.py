209583f446b28bb28d3f41bc206ad10cf301856f
Vesk <avesky@gmail.com>
2025-10-22 20:10:30 -0500

    code from A side

--- tasks/taskA.py
+++ tasks/taskA.py
@@ -1,4 +1,7 @@
 from init import robot,lift,pressure
+from utilities.lift import reset_lift, lift_to_height
+from pybricks.tools import multitask
+
 
 # async def square():
 #     await robot.straight(400)
@@ -14,24 +17,33 @@ from init import robot,lift,pressure
 #     await robot.straight(400)
 #     await robot.turn(-90)
 
-async def liftGoDown():
-    await lift.run_target (400,0)
-async def task():
-    #await robot.turn(360)
+async def doChallenge1():
     robot.settings(straight_speed=200)
     
     # await lift.run_angle(360,360)
-
-    await lift.run_target(400,-950,)
-    await robot .straight(60)
+    await multitask(
+        reset_lift(),
+        robot.straight(200)
+    )
+    # await reset_lift()
+    # await robot .straight(200)
+    await robot.curve(400,57)
+    await robot.turn (-140)
+    await robot .straight(120)
+    await lift_to_height(850)
+    await robot .straight(70)
     await robot.turn (30)
     await robot.turn (-60)
     await robot.turn (30)
     await robot.straight (-60)
-    await liftGoDown()
-    # await robot.curve(300,-80)
-    # await square()
+    await reset_lift()
 
-    # await square()
-    # await square()
+async def liftGoDown():
+    await lift.run_target (400,0)
+async def task():
+    #await robot.turn(360)
+    await doChallenge1()
+
+async def drive2():
+    await robot.curve(350,-15)
     
